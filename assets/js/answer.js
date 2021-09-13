c7de8fc050b0d6391265b3b83032449ed00e62fc
aa667248 <89883809+aa667248@users.noreply.github.com>
2021-09-13 12:10:33 +0900

    Update answer.js

--- assets/js/answer.js
+++ assets/js/answer.js
@@ -34,7 +34,7 @@ function Textform(value){
         if(ans==aa[3]){
             document.getElementById('all').innerHTML='';
             document.getElementById('all').insertAdjacentHTML('afterbegin','<p>夜になればわかる<p>');
-            document.getElementById('all').insertAdjacentHTML('afterbegin','<p id="password">password:<br>ここに答え</p>');
+            document.getElementById('body_all').insertAdjacentHTML('afterbegin','<p id="password">password:<br>ここに答え</p>');
             document.getElementById('dark_mode').remove();
             document.getElementById('switch').insertAdjacentHTML('afterbegin','<input type="checkbox" id="dark_mode" onchange="changesytle()">');
             console.log('stage4:clear')
