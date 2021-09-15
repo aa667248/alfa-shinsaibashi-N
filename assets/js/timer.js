7f58c39009ca2cff195c53e64ee7cfeec0516a75
aa667248 <89883809+aa667248@users.noreply.github.com>
2021-09-15 11:02:44 +0900

    Update timer.js

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -12,7 +12,7 @@ function Textform(){
   var ans = document.getElementById('textbox').value;
   var aa = 'かいじょ';
   var ss = 'U2FsdGVkX1/0IpPcqsWAyEt5j8hGJV1y1rbSaWSdX02KMJBWhwYH80uFzT+NQU92tXibnvS290poZX0dxSjk4t9p+Gh4mToEe2+VS2hnhZ9KTiTlccDZu1wspys4z6IBHPebP+PQ7KJtEItPvec79S+QFEa9ooZWNRETrEvin1cDDwEQwZI/yceDAc5Fp1af'/**aes:I wrote that you must not look at the source code*/';
-  var exc = 'ほりの'
+  var exc = 'ほりのけんた'
   if(ans == aa){
       document.getElementById('time').innerHTML = 'clear';
       stageclear()
